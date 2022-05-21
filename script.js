79020bd76161ccbdfa06361cdc8337c32331a544
ThokozaniMakgopa <Thokozanimakgopa@gmail.com>
2022-05-21 17:18:58 +0200

    new javascript

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function showMenu(){
 function hideMenu(){
       navLinks.style.right = "-200px";
 }
+
+var layer = document.getElementsByClassName("layer")[0];
+
+layer.addEventListener("touchstart", function(){
+    layer.classList.add("layer:hover");
+})
+
+layer.addEventListener("touchend", function(){
+    layer.classList.remove("layer:hover");
+})
